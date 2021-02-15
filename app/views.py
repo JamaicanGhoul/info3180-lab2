163a92e7a6a96f7561c88dfd5bbde693ea9c72a8
Javier Stewart <javierlamarstewart68@gmail.com>
2021-02-14 22:58:11 -0500

    your commit message

--- app/views.py
+++ app/views.py
@@ -7,50 +7,33 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from datetime import date
 
 ###
 # Routing for your application.
 ###
 
+@app.route('/profile')
+def profile():
+    "This shows the profile"
+    date_joined = date(2021, 2, 8)
+    return render_template('profile.html',date=format_date_joined(date_joined))
+
+
 @app.route('/')
 def home():
     """Render website's home page."""
-    return render_template('home.html')
-
-
-@app.route('/about/')
-def about():
+@@ -24,6 +32,9 @@ def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Javier Stewart")
+
+def format_date_joined(date):
+    return "Joined "  + date.strftime("%B, %Y")
 
 
 ###
 # The functions below should be applicable to all Flask apps.
-###
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send your static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
-
-
-@app.after_request
-def add_header(response):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also tell the browser not to cache the rendered page. If we wanted
-    to we could change max-age to 600 seconds which would be 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'public, max-age=0'
-    return response
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    """Custom 404 page."""
+@@ -54,5 +65,9 @@ def page_not_found(error):
     return render_template('404.html'), 404
 
 
